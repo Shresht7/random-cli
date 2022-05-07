790a361148af39c5b79bfc3b3a4e6fa98ff61f67
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2022-05-07 23:27:57 +0530

    Read arguments and select one at random

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use rand::{self, Rng};
+
 fn main() {
-    println!("Hello, world!");
+    //  Collect the parsed arguments in a vector
+    let args: Vec<String> = std::env::args().collect::<Vec<String>>()[1..].to_vec();
+
+    println!("{:#?}", args);
+
+    let mut rng = rand::thread_rng();
+    let selection = rng.gen_range(0..args.len());
+
+    println!("{}: {}", selection, args[selection]);
 }
