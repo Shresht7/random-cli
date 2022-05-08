b510b20d5a3ac02ccad5d4f7d9a6d9d479773291
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2022-05-08 13:42:16 +0530

    Componentize get_random_number helpers

--- src/commands/number.rs
+++ src/commands/number.rs
@@ -2,12 +2,18 @@ use rand::{prelude::ThreadRng, Rng};
 
 pub fn number(num1: i32, num2: Option<i32>, rng: &mut ThreadRng) {
     let result: i32 = match num2 {
-        Some(num2) => {
-            let low = std::cmp::min(num1, num2);
-            let max = std::cmp::max(num1, num2);
-            rng.gen_range(low..max)
-        }
-        None => rng.gen_range(0..num1.to_owned()),
+        Some(num2) => get_random_number_between(num1, num2, rng),
+        None => get_random_number(num1, rng),
     };
     println!("{}", result);
+}
+
+fn get_random_number_between(num1: i32, num2: i32, rng: &mut ThreadRng) -> i32 {
+    let low = std::cmp::min(num1, num2);
+    let max = std::cmp::max(num1, num2);
+    return rng.gen_range(low..max);
+}
+
+fn get_random_number(num1: i32, rng: &mut ThreadRng) -> i32 {
+    return rng.gen_range(0..num1);
 }
