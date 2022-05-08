3afadf3f2153af4a5d4d23b40d60c8dfaf232b6d
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2022-05-09 01:23:20 +0530

    Random select accepts piped inputs

--- src/commands/select.rs
+++ src/commands/select.rs
@@ -1,6 +1,21 @@
-use rand::{Rng};
+use std::io::{Read};
+
+use rand::Rng;
 
 pub fn select(entries: &Vec<String>) {
+    //  Clone a mutable shadow of entries
+    let mut entries = entries.clone();
+
+    //  Read input from stdin
+    let mut lines = String::new();
+    std::io::stdin().read_to_string(&mut lines).unwrap();
+    for line in lines.lines() {
+        entries.push(String::from(line));
+    }
+
+    //  Select one entry at random
     let selection = rand::thread_rng().gen_range(0..entries.len());
-    println!("{}: {}", selection, entries[selection]);
+
+    //  Show results
+    println!("{}", entries[selection]);
 }
