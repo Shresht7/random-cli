ddee826819b4196d512e8bb28bd122904862482e
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2023-11-11 23:34:43 +0530

    Upgrade from `atty` to `is-terminal`

--- src/lib/helpers/read_stdin.rs
+++ src/lib/helpers/read_stdin.rs
@@ -1,15 +1,21 @@
 //  Library
+use is_terminal::IsTerminal;
 use std::io::Read;
 
 /// Read Stdin and append lines to the given vector
 pub fn read_stdin_into(vector: &mut Vec<String>) {
-    if atty::is(atty::Stream::Stdin) {
+    // Standard input
+    let mut stdin = std::io::stdin();
+
+    //  Return early if stdin is from a tty
+    if stdin.is_terminal() {
         return;
     }
+
     //  Read input from stdin
     let mut lines = String::new();
     //  Read stdin only if redirected using a pipe
-    std::io::stdin().read_to_string(&mut lines).unwrap();
+    stdin.read_to_string(&mut lines).unwrap();
     //  Push to entries
     for line in lines.lines() {
         vector.push(String::from(line));
