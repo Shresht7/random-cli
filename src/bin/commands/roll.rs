100ea7b388cf835b302bb289c0ed9774a88c752e
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2023-11-13 17:17:41 +0530

    Add support for die modifiers (`3d8 + 5`) Fixes #21

--- src/bin/commands/roll.rs
+++ src/bin/commands/roll.rs
@@ -8,18 +8,21 @@ use random::die;
 
 /// Roll die
 ///
-/// Roll die accepts input in the {n}d{S} format; where {n} is the number of dice thrown
-/// and {S} is the number of sides each die has. (example: 3d8 - Three 8-sided die)
+/// Roll die accepts input in the {n}d{S}+{X} format; where {n} is the number of dice thrown
+/// and {S} is the number of sides each die has and {X} is the number to add to the result.
+/// (example: 3d8 - Three 8-sided die, 2d6+4 - Two 6-sided dice and add 4 to the result)
 /// defaults to 1d20
 ///
 /// Examples:
-/// random die          -   Rolls one twenty-sided die
-/// random die 1d6      -   Rolls one six-sided die
-/// random die 3d12     -   Rolls three twelve-sided die
+/// random roll             -   Rolls one twenty-sided die
+/// random roll 1d6         -   Rolls one six-sided die
+/// random roll 3d12        -   Rolls three twelve-sided dice
+/// random roll 1d8+5       -   Rolls a eight-sided die and adds 5 to the result
+/// random roll 1d8+2d6+3   -   Rolls an eight sided die, two six-sided dice and adds 3 to the result
 #[derive(Args)]
 #[clap(verbatim_doc_comment)]
 pub struct Roll {
-    /// The dice to roll in {n}d{S} format
+    /// The dice to roll
     die: Option<String>,
 
     /// Roll the die with advantage. Take the greatest value
@@ -43,32 +46,57 @@ impl Roll {
             None => String::from("1d20"),
         };
 
-        //  Roll the die and determine the total
-        let mut result = die::roll(&die);
-        let mut total: &u8 = &result.iter().sum::<u8>();
+        // Extract the terms
+        let terms = die.split("+").map(|x| x.trim()).collect::<Vec<&str>>();
 
-        //  Show results
-        println!("Rolls: {:?} = {}", result, total);
+        // Determine rolls, additions and the total
+        let mut total: u8 = 0;
+        for term in &terms {
+            // If this is a dice term ...
+            if term.to_lowercase().contains("d") {
+                // Roll the dice
+                let mut roll = die::roll(&term);
 
-        result.sort(); // Sort the results to easily determine advantage and disadvantage
+                if &terms[0] != term {
+                    print!("\n");
+                } // Print newline for everything except the first
+                print!("Roll {}: {:?} ", term, roll); // Print the Roll
 
-        // If rolling with advantage...
-        if self.with_advantage {
-            total = result.last().unwrap();
-            println!("With Advantage: {}", total);
+                // If the roll was with advantage ...
+                if self.with_advantage {
+                    // Determine the max roll
+                    roll.sort();
+                    let roll = roll.last().unwrap_or(&0);
+                    print!("-- Advantage --> {} ", roll);
+                    total += roll;
+                // ... else if the roll was with disadvantage ...
+                } else if self.with_disadvantage {
+                    // Determine the min roll
+                    roll.sort();
+                    let roll = roll.first().unwrap_or(&0);
+                    print!("-- Disadvantage --> {} ", roll);
+                    total += roll;
+                // ... otherwise, use the rolls as is
+                } else {
+                    total += &roll.iter().sum::<u8>();
+                }
+            // ... else if this is an addition
+            } else {
+                print!("+ {}", term);
+                total += term.parse::<u8>().unwrap_or_default();
+            }
         }
 
-        // If rolling with disadvantage...
-        if self.with_disadvantage {
-            total = result.first().unwrap();
-            println!("With Disadvantage: {}", total);
-        }
+        // Print the total
+        print!("\nTotal: {} ", total);
 
-        // If a difficulty check is present, print Success or Failure based on the results
+        // If the difficulty check option was passed
         if self.difficulty_check > 0 {
-            println!("Difficulty: {}", self.difficulty_check);
-            if total >= &self.difficulty_check {
+            print!("(Difficulty: {}) ", self.difficulty_check);
+            // If the total beats the difficulty check, print success ...
+            if total >= self.difficulty_check {
                 println!("Success")
+            // ...otherwise, print failure.
             } else {
                 println!("Failure")
             }
